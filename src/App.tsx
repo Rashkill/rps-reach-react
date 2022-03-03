686e47c4ef5042239286609e74e1fd15d3c5fbfd
Juan Becchio <rashkill@hotmail.com>
2022-03-03 12:12:48 -0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HandSelector from "./views/HandSelector";
+import RoleSelector from "./views/RoleSelector";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/hands" element={<HandSelector />} />
+          <Route path="/" element={<RoleSelector />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/views/RoleSelector/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+const RoleSelector = () => {
+  return (
+    <div>
+      <h1>Role Selector</h1>
+      <div className="options">
+        <div className="optionItem">Alice</div>
+        <div className="optionItem">Bob</div>
+      </div>
+    </div>
+  );
+};
+
+export default RoleSelector;
